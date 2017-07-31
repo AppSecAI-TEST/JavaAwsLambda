2a44a13104a59965046ac392bcf5b6d39b8660c2
npatrick96 <patini2020@yahoo.fr>
2017-07-31 16:27:58 -0400

    changed testfile location

--- src/test/java/com/gcit/AppTest.java
+++ src/test/java/com/gcit/AppTest.java
@@ -27,8 +27,7 @@ public class AppTest {
 	 */
 	@Test
 	public void appTest() throws FileNotFoundException {
-		File initialFile = new File(
-				"/Users/akbarhosseinkhani/Documents/GCIT/Code/eclipse-workspace/lambda-java-example/src/test/java/com/gict/testFile.txt");
+		File initialFile = new File("testFile.txt");
 		InputStream targetStream = new FileInputStream(initialFile);
 		String test = "{\r\n    \"context\": {\r\n        \"authorizer-principal-id\": \"\",\r\n        \"cognito-authentication-type\": \"\",\r\n        \"cognito-identity-id\": \"\",\r\n        \"resource-path\": \"/lms/authors/{authorId}\",\r\n        \"account-id\": \"985863529922\",\r\n        \"cognito-identity-pool-id\": \"\",\r\n        \"request-id\": \"test-invoke-request\",\r\n        \"api-id\": \"ffstl2zfs1\",\r\n        \"resource-id\": \"scif8o\",\r\n        \"user-arn\": \"arn:aws:iam::985863529922:root\",\r\n        \"caller\": \"985863529922\",\r\n        \"http-method\": \"GET\",\r\n        \"cognito-authentication-provider\": \"\",\r\n        \"api-key\": \"test-invoke-api-key\",\r\n        \"stage\": \"test-invoke-stage\",\r\n        \"source-ip\": \"test-invoke-source-ip\",\r\n        \"user\": \"985863529922\",\r\n        \"user-agent\": \"Apache-HttpClient/4.5.x (Java/1.8.0_112)\"\r\n    },\r\n    \"body-json\": {\"authorId\": \"3\",\"authorName\":\"aloi\"},\r\n    \"params\": {\r\n        \"path\": {\r\n            \"authorId\": \"3\"\r\n        },\r\n        \"querystring\": {},\r\n        \"header\": {}\r\n    },\r\n    \"stage-variables\": {}\r\n}";
 		Hello hello = new Hello();
